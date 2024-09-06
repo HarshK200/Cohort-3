f588ca43b0cbbbaebc3b3668ab921288682e69b6
HarshK200 <harshkapadia04@gmail.com>
2024-09-06 16:19:45 +0530

    basic todo box structure done

--- web_development/week3-DOM/Taskify/components/CategoriesContainer.js
+++ web_development/week3-DOM/Taskify/components/CategoriesContainer.js
@@ -1,28 +1,65 @@
 const CategoriesContainer = (CATEGORIES, TODOS, reRender) => {
-  const todosDiv = document.createElement("div");
-  todosDiv.classList.add("todos-container");
+  const categoriesContainer = document.createElement("div");
+  categoriesContainer.classList.add("categories-container");
 
   Object.keys(CATEGORIES).forEach((key) => {
-    todosDiv.appendChild(Category(CATEGORIES[key], TODOS, reRender));
+    categoriesContainer.appendChild(Category(CATEGORIES[key], TODOS, reRender));
   });
 
-  return todosDiv;
+  return categoriesContainer;
 };
 
-const Category = (title, TODOS, reRender) => {
+const Category = (category, TODOS, reRender) => {
   const categoryDiv = document.createElement("div");
   categoryDiv.classList.add("category-div");
 
   const categoryTitle = document.createElement("h3");
-  categoryTitle.textContent = title;
+  categoryTitle.textContent = category;
 
+  const todosDiv = document.createElement("div");
+  todosDiv.classList.add("todos-container");
+  Object.keys(TODOS).forEach((key) => {
+    if (TODOS[key].category == category)
+      todosDiv.appendChild(Todo(TODOS[key], reRender));
+  });
+  Object.keys((key) => {
+    todosDiv.appendChild(Todo(TODOS[key], reRender));
+  });
 
+  const addTodoBtn = document.createElement("button");
+  addTodoBtn.classList.add("addTodoBtn");
+  addTodoBtn.textContent = "Add Todo";
+  addTodoBtn.onclick = () => {};
 
   categoryDiv.appendChild(categoryTitle);
+  categoryDiv.appendChild(todosDiv);
+  categoryDiv.appendChild(addTodoBtn);
 
   return categoryDiv;
 };
 
-const Todo = (TODO, reRender) => {};
+const Todo = (TODO, reRender) => {
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo-div");
+  todoDiv.draggable = true;
+
+  const textContainer = document.createElement("div");
+
+  const title = document.createElement("h2");
+  title.textContent = TODO.title;
+  const content = document.createElement("p");
+  content.textContent = TODO.content;
+
+  textContainer.appendChild(title);
+  textContainer.appendChild(content);
+
+  const timestamp = document.createElement("p");
+  timestamp.textContent = TODO.timestamp;
+
+  todoDiv.appendChild(textContainer);
+  todoDiv.appendChild(timestamp);
+
+  return todoDiv;
+};
 
 export default CategoriesContainer;
--- web_development/week3-DOM/Taskify/main.js
+++ web_development/week3-DOM/Taskify/main.js
@@ -1,8 +1,47 @@
 import App from "./App.js";
 
 // state
-const CATEGORIES = { 0: "To Do", 1: "In Progress", 2: "Completed", 3: "On Hold" };
-const TODOS = [{ id: 1, title: "foo", content: "bar", category: "Todo" }];
+const CATEGORIES = {
+  0: "To Do",
+  1: "In Progress",
+  2: "Completed",
+  3: "On Hold",
+};
+const options = { year: "numeric", month: "short", day: "numeric" };
+const TODOS = {
+  0: {
+    id: 1,
+    title: "foo",
+    content: "fdsklfhdslkhf",
+    category: "To Do",
+    urgency: "low",
+    timestamp: new Date().toLocaleDateString("en-US", options),
+  },
+  1: {
+    id: 2,
+    title: "test",
+    content: "adlkfjasklfj",
+    category: "To Do",
+    urgency: "high",
+    timestamp: new Date().toLocaleDateString("en-US", options),
+  },
+  2: {
+    id: 3,
+    title: "test2",
+    content: "adlkfjasklfj",
+    category: "In Progress",
+    urgency: "low",
+    timestamp: new Date().toLocaleDateString("en-US", options),
+  },
+  3: {
+    id: 4,
+    title: "test3",
+    content: "adlkfjasklfj",
+    category: "On Hold",
+    urgency: "low",
+    timestamp: new Date().toLocaleDateString("en-US", options),
+  },
+};
 
 function reRender() {
   render(App(CATEGORIES, TODOS, reRender));
